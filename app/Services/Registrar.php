c4190635041171f14363b71d1be55116c99adca0
Sourav Das <souravdas4816@gmail.com>
2024-01-15 13:09:21 +0530

    Upgraded to Laravel 5.1.x

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -1,10 +1,13 @@
-<?php namespace App\Services;
+<?php
+
+namespace App\Services;
 
 use App\User;
-use Validator;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Contracts\Auth\Registrar as RegistrarContract;
 
-class Registrar implements RegistrarContract {
+class Registrar implements RegistrarContract
+{
 
 	/**
 	 * Get a validator for an incoming registration request.
@@ -35,5 +38,4 @@ class Registrar implements RegistrarContract {
 			'password' => bcrypt($data['password']),
 		]);
 	}
-
 }
